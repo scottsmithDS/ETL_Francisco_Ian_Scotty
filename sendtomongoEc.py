492f84c19c6f8d9749edb587d11e1d8829a203f2
scottsmithDS <shsmithsd@gmail.com>
2020-04-30 22:35:56 -0700

    u

---
+++ sendtomongoEc.py
@@ -0,0 +1,11 @@
+import pymongo
+import csv
+import json
+from pymongo import MongoClient 
+
+import pandas as pd
+with open('Ecuador.json') as f:
+    file_data = json.load(f)
+client = MongoClient('mongodb://admin:new_password_here@127.0.0.1:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false')
+db = client['INTL_db']
+db["audits"].insert_one(file_data)
